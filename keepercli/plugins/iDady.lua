5fc7b4aa86741399e22b043f69776f49802bf49c
Ali3aber <48875846+Ali3aber@users.noreply.github.com>
2019-11-04 21:57:42 +0330

    Update iDady.lua

--- keepercli/plugins/iDady.lua
+++ keepercli/plugins/iDady.lua
@@ -21,3 +21,23 @@ return {
       }, 
   run = run
 }
+local function run(msg, matches)
+if matches[1] == "علی صابر" then
+      if msg.to.type == 'channel' or 'chat' then
+            local answers = {'🥰😍وای باباییم😍😍','باباییه من'}
+            return answers[math.random(#answers)]
+      end
+	   end
+     	  if matches[1] == "alisaber" then
+      if msg.to.type == 'channel' or 'chat' then
+           local answers = {'وایی عشقم بابام','با بابام کاری داری؟','عشق منهه که','دوست دارم بابایی','بابایی بیا کارت دارن','واییی باباییییی','بوج برای بابام'}
+            return answers[math.random(#answers)]
+      end
+	  end
+    end
+return
+  patterns = {
+  "alisaber$",
+  "علی صابر$"
+      }
+  run = run
